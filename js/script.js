2111b00240e3e7a8e64082cbc7b70d3230c84d47
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2017-04-20 13:02:44 +0100

    rough version
    modified the Norway site and reused code from the previous greek site

---
+++ js/script.js
@@ -0,0 +1,133 @@
+(function ($, root, undefined) {$(function () {'use strict'; // on ready start
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////
+//        general
+///////////////////////////////////////
+
+  // css tricks snippet - http://css-tricks.com/snippets/jquery/smooth-scrolling/
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  });
+
+  // inserts current year
+  $('.js-year').html(new Date().getFullYear());
+
+  // detects touch device
+  if ("ontouchstart" in document.documentElement){
+    $('html').addClass('touch');
+  }
+
+
+///////////////////////////////////////
+//        Navigation
+///////////////////////////////////////
+
+  // mobile nav toggle open & close
+  $('.js-toggle-mobile-nav').on('click', function(e) {
+    $('.mobile-nav').toggleClass('is-open').toggleClass('is-closed');
+  });
+
+  // current page nav highlight
+  var currentPage     = $('body').data('current-page'),
+      currentCategory = $('body').data('current-category');
+
+  // add class to individual nav item
+  $('.page--' + currentPage + ' [class*=nav__item--' + currentPage + ']').addClass('is-current');
+
+  // if there is a category, add class to category nav item
+  if (currentCategory !== ''){
+    $('.category--' + currentCategory + ' [class*=nav__item--' + currentCategory + ']').addClass('is-current');
+  }
+
+
+///////////////////////////////////////
+//      SVG image swap
+///////////////////////////////////////
+
+  // finds image with class and swaps .png with .svg in img source string
+  if (Modernizr.svgasimg) {
+    var svgSwap = $('img.js-svg-swap');
+    svgSwap.each( function() {
+      var currentSrc = $(this).attr('src'),
+          newSrc = currentSrc.replace('.png', '.svg');
+      $(this).attr('src', newSrc);
+    });
+  }
+
+
+///////////////////////////////////////
+//      Youtube thumbnails
+///////////////////////////////////////
+
+  // stopped on touch devices
+  if ( $('html.touch').length === 0 ) {
+
+    // Loops through all videos on page
+    $('.js-youtube-thumbnail').each(function(index, el) {
+      var video             = $(this).find('.video__iframe'),
+          videoSrc          = video.attr('src'),
+          thumbnailImg      = $(this).data('thumbnail-img'),
+          thumbnailElement  = '<div class="video__thumbnail" style="background-image: url(\'' + thumbnailImg + '\')"><div class="video__play js-play-video"></div></div>';
+
+      // hide video, but keep aspect ratio
+      video.css('visibility', 'hidden');
+
+      // Add thumbnail element to hold image & play button
+      $(this).prepend(thumbnailElement);
+      var thumbnail   = $(this).find('.video__thumbnail'),
+          playButton  = $(this).find('.js-play-video');
+
+      // play button event
+        playButton.on('click', function(e) {
+          e.preventDefault();
+          // add auto play query to iframe
+          video.attr('src', videoSrc + '&autoplay=1');
+          // fade out iframe and show video
+          thumbnail.fadeOut( 175, function() {
+            video.css('visibility', 'visible');
+          });
+        });
+
+    });
+
+  }
+
+
+///////////////////////////////////////
+//       Offer expiry countdown
+///////////////////////////////////////
+
+// loops through each offer on page and sets the current days remaining
+$('.js-offer-expires').each(function() {
+  // gets the expires date from the object
+  var expires = $(this).data('expires');
+  $(this).countdown(expires, function(event) {
+    if (event.elapsed) {
+      // the expired date is in the past so the expired message is removed
+      $(this).remove();
+    } else if (event.offset.totalDays === 0) {
+      // there is 0 days left, just hours, so ends today
+      $(this).html(event.strftime('Ending <strong>Today</strong>'));
+    } else {
+      // there are days left, outputs with either day or days
+      $(this).html(event.strftime('Ending in <strong>%-D day%!D</strong>'));
+    }
+  });
+});
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+});})(jQuery, this); // on ready end
